c16be35acd674f0f0eee680f74edc64546ddf74c
ashraf <ashraf1abdelrasool@gmail.com>
2018-12-15 08:14:09 +0200

    push all

--- src/main/java/org/revo/Util/Util.java
+++ src/main/java/org/revo/Util/Util.java
@@ -11,11 +11,6 @@ import java.util.function.Function;
  * Created by ashraf on 16/04/17.
  */
 public class Util {
-    public final static String stream = "#EXTM3U\n" +
-            "# Created with Bento4 mp4-hls.py version 1.1.0r621\n" +
-            "\n" +
-            "# Media Playlists\n";
-
 
     public static String generateKey() {
         return UUID.randomUUID().toString().replace("-", "");
@@ -30,30 +25,5 @@ public class Util {
                 builder.append("\n").append(function.apply(tag.getURI()));
         }
         return builder.toString();
-    }
-
-    public static byte[] secret(String encryption_key) {
-        final int len = encryption_key.length();
-        byte[] out = new byte[len / 2];
-        for (int i = 0; i < len; i += 2) {
-            int h = hexToBin(encryption_key.charAt(i));
-            int l = hexToBin(encryption_key.charAt(i + 1));
-
-            out[i / 2] = (byte) (h * 16 + l);
-        }
-        return out;
-    }
-
-    private static int hexToBin(char ch) {
-        if ('0' <= ch && ch <= '9') {
-            return ch - '0';
-        }
-        if ('A' <= ch && ch <= 'F') {
-            return ch - 'A' + 10;
-        }
-        if ('a' <= ch && ch <= 'f') {
-            return ch - 'a' + 10;
-        }
-        return -1;
     }
 }
