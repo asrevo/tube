752222319e6c111d1154cfa1e4605e4c7d0dfeca
ashraf <ashraf1abdelrasool@gmail.com>
2019-01-12 21:30:47 +0200

    push all

--- src/main/java/org/revo/Config/SecurityConfig.java
+++ src/main/java/org/revo/Config/SecurityConfig.java
@@ -7,7 +7,14 @@ import org.springframework.data.domain.AuditorAware;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import java.util.Collection;
 
 /**
  * Created by ashraf on 18/04/17.
@@ -25,7 +32,13 @@ public class SecurityConfig {
                 .anyExchange().authenticated()
                 .and()
                 .oauth2ResourceServer()
-                .jwt().and().and().build();
+                .jwt()
+                .jwtAuthenticationConverter(new ReactiveJwtAuthenticationConverterAdapter(new JwtAuthenticationConverter() {
+                    protected Collection<GrantedAuthority> extractAuthorities(Jwt jwt) {
+                        return AuthorityUtils.createAuthorityList(((String[]) jwt.getClaims().get("authorities")));
+                    }
+                }))
+                .and().and().build();
     }
 
     @Bean
