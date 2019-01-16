5c71351f2f3f2c8562db8105de1c03d5a8584d78
ashraf <ashraf1abdelrasool@gmail.com>
2019-01-16 12:33:34 +0200

    push all

--- src/main/java/org/revo/Controller/FileController.java
+++ src/main/java/org/revo/Controller/FileController.java
@@ -8,16 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
-import reactor.core.publisher.Mono;
 
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
-import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 //@RestController
 //@RequestMapping("api/file")
@@ -33,8 +27,7 @@ public class FileController {
     public RouterFunction<ServerResponse> function() {
         return route()
                 .POST("api/file/save", serverRequest -> {
-                    Mono<Void> p = serverRequest.bodyToMono(File.class).flatMap(it -> fileService.save(it)).then();
-                    return ok().build(p);
+                    return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(serverRequest.bodyToMono(File.class), File.class);
                 })
                 .build();
     }
