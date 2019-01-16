e206274faf9289732f1913108cab0eb4f95c8dd3
ashraf <ashraf1abdelrasool@gmail.com>
2019-01-16 22:16:08 +0200

    push all

--- src/main/java/org/revo/Service/Receiver.java
+++ src/main/java/org/revo/Service/Receiver.java
@@ -2,15 +2,14 @@ package org.revo.Service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.revo.Config.Processor;
-import org.revo.Domain.File;
 import org.revo.Domain.Index;
 import org.revo.Domain.Master;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.integration.annotation.MessageEndpoint;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.SendTo;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 /**
@@ -32,22 +31,17 @@ public class Receiver {
 
     @StreamListener(value = Processor.tube_info)
     @SendTo(value = Processor.feedback_index)
-    public Mono<Master> info(Message<Master> master) {
-        log.info("receive tube_info " + master.getPayload().getId());
-        return masterService.saveInfo(master.getPayload());
+    public Flux<Master> info(Mono<Master> master) {
+        log.info("receive tube_info ");
+        return master.flatMapMany(it -> masterService.saveInfo(it));
     }
 
     @StreamListener(value = Processor.tube_store)
     @SendTo(value = Processor.ffmpeg_queue)
-    public Mono<Master> store(Message<Master> master) {
-        log.info("receive tube_store " + master.getPayload().getId());
-        Mono<Master> save = masterService.save(master.getPayload());
-//        log.info("send ffmpeg_queue " + save.getId());
+    public Flux<Master> store(Mono<Master> master) {
+        log.info("receive tube_store ");
+        Flux<Master> save = master.flatMapMany(it -> masterService.save(it));
+        log.info("send ffmpeg_queue ");
         return save;
-    }
-
-    @SqsListener("tube_file_queue")
-    public void queueListener(Message<File> fileMessage) {
-        log.info("receive tube_file_queue " + fileMessage.getPayload().getId());
     }
 }
