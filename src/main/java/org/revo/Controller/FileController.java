be6bcd21432614320924d17e484c9bf7325cd33b
ashraf <ashraf1abdelrasool@gmail.com>
2019-01-16 14:13:04 +0200

    push all

--- src/main/java/org/revo/Controller/FileController.java
+++ src/main/java/org/revo/Controller/FileController.java
@@ -7,11 +7,14 @@ import org.revo.Service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
-import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+import static org.springframework.web.reactive.function.server.RouterFunctions.nest;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
@@ -27,7 +30,15 @@ public class FileController {
 
     @Bean
     public RouterFunction<ServerResponse> function() {
-        return route(POST("api/file/save"), serverRequest -> ok().body(serverRequest.bodyToMono(File.class).flatMap(it -> fileService.save(it))
+
+//        return nest(path("/person"),
+//                nest(accept(APPLICATION_JSON),
+//                        route(GET("/{id}"), handler::getPerson)
+//                                .andRoute(method(HttpMethod.GET), handler::allPeople).andRoute(POST("/").and(contentType(APPLICATION_JSON)), handler::savePerson))
+//                );
+//
+        return route(
+                POST("/api/file/save"), serverRequest -> ok().body(serverRequest.bodyToMono(File.class).flatMap(it -> fileService.save(it))
                 .doOnNext(it -> processor.file_queue().send(MessageBuilder.withPayload(it).build()))
                 .then(), Void.class)
 
