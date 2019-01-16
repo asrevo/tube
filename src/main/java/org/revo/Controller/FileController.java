8a849b0cd05c2d509d29fa4d9a0230143b1457f8
ashraf <ashraf1abdelrasool@gmail.com>
2019-01-16 11:14:57 +0200

    push all

--- src/main/java/org/revo/Controller/FileController.java
+++ src/main/java/org/revo/Controller/FileController.java
@@ -5,16 +5,12 @@ import org.revo.Config.Processor;
 import org.revo.Domain.File;
 import org.revo.Service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.integration.support.MessageBuilder;
-import org.springframework.messaging.Message;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
-
-//import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("api/file")
@@ -26,9 +22,9 @@ public class FileController {
     public Processor processor;
 
     @PostMapping("save")
-    public Mono<Message<File>> save(@RequestBody File file, ServerHttpRequest request) {
+    public Mono<Void> save(@RequestBody File file) {
         return fileService.save(file).map(MessageBuilder::withPayload)
                 .map(MessageBuilder::build)
-                .doOnEach(it -> processor.file_queue().send(it.get()));
+                .doOnNext(it -> processor.file_queue().send(it)).then();
     }
 }
