50961a597c0dd55c5ae6213f517f8b3eb96235f1
ashraf <ashraf1abdelrasool@gmail.com>
2019-01-16 14:29:02 +0200

    push all

--- src/main/java/org/revo/Controller/FileController.java
+++ src/main/java/org/revo/Controller/FileController.java
@@ -7,11 +7,12 @@ import org.revo.Service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RequestPredicates.path;
+import static org.springframework.web.reactive.function.server.RouterFunctions.nest;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
@@ -36,15 +37,15 @@ public class FileController {
 //
 
 
-        //        return nest(path(""),route(GET(""),it->null)).andNest(path(""),route(GET(""),it->null));
+        return nest(path("/api/file"), route(POST("/save"), serverRequest -> ok().body(serverRequest.bodyToMono(File.class).flatMap(it -> fileService.save(it)), File.class)));
 
 
-        return route(
-                POST("/api/file/save"), serverRequest -> ok().body(serverRequest.bodyToMono(File.class).flatMap(it -> fileService.save(it))
-//                        .doOnNext(it -> processor.file_queue().send(MessageBuilder.withPayload(it).build()))
-                        .then(), Void.class)
-
-        );
+//        return route(
+//                POST("/api/file/save"), serverRequest -> ok().body(serverRequest.bodyToMono(File.class).flatMap(it -> fileService.save(it))
+////                        .doOnNext(it -> processor.file_queue().send(MessageBuilder.withPayload(it).build()))
+//                        .then(), Void.class)
+//
+//        );
     }
 
 }
