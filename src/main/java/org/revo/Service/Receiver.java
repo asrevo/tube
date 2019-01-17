0478d7978d9d58606288df7e6dc1b6ce36f07a1c
ashraf <ashraf1abdelrasool@gmail.com>
2019-01-17 08:40:58 +0200

    push all

--- src/main/java/org/revo/Service/Receiver.java
+++ src/main/java/org/revo/Service/Receiver.java
@@ -5,10 +5,10 @@ import org.revo.Config.Processor;
 import org.revo.Domain.Index;
 import org.revo.Domain.Master;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.Input;
 import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.integration.annotation.MessageEndpoint;
-import org.springframework.messaging.Message;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -23,26 +23,25 @@ public class Receiver {
     @Autowired
     private IndexService indexService;
 
-    @StreamListener(value = Processor.tube_hls)
-    public void hls(Message<Index> index) {
+    @StreamListener
+    public void hls(@Input(Processor.tube_hls) Flux<Index> index) {
         log.info("receive tube_hls ");
-        indexService.save(index.getPayload()).
-                flatMapMany(it -> masterService.append(Mono.just(it))).subscribe();
+        index.flatMap(it -> indexService.save(it)).
+                flatMap(it -> masterService.append(Mono.just(it))).subscribe();
     }
 
-    @StreamListener(value = Processor.tube_info)
-    @Output(value = Processor.feedback_index)
-    public Flux<Master> info(Message<Master> master) {
+    @StreamListener
+    @Output(Processor.feedback_index)
+    public Flux<Master> info(@Input(Processor.tube_info) Flux<Master> master) {
         log.info("receive tube_info ");
-        return masterService.saveInfo(master.getPayload())
-                .flatMapMany(Mono::just);
+        return master.flatMap(it -> masterService.saveInfo(it));
     }
 
-    @StreamListener(value = Processor.tube_store)
-    @Output(value = Processor.ffmpeg_queue)
-    public Flux<Master> store(Message<Master> master) {
+    @StreamListener
+    @Output(Processor.ffmpeg_queue)
+    public Flux<Master> store(@Input(Processor.tube_store) Flux<Master> master) {
         log.info("receive tube_store ");
-        Flux<Master> save = masterService.save(master.getPayload()).flatMapMany(Mono::just);
+        Flux<Master> save = master.flatMap(it -> masterService.save(it));
         log.info("send ffmpeg_queue ");
         return save;
     }
